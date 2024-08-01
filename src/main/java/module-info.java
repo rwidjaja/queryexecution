e8272d0c5cc70ab33c942df930378bace8690246
Rudy Widjaja <rwidjaja@hotmail.com>
2024-08-01 09:35:54 -0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.ubuntu {
+    requires java.xml;
+    requires jakarta.xml.soap;
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
-
-    opens com.ubuntu to javafx.fxml;
+    requires jakarta.activation; 
     exports com.ubuntu;
 }
